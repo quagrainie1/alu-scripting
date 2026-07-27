ade8730dede41366be8a543fac8d2c073bb7763c
quagrainie1 <e.quagraini@alustudent.com>
2026-07-27 09:09:40 +0200

    Fix 0-subs.py implementation

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """
-0-main
+0-subs
 """
-import sys
+import requests
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If the subreddit is invalid, returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    try:
+        return response.json().get("data", {}).get("subscribers", 0)
+    except ValueError:
+        return 0
