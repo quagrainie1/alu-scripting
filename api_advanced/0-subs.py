1474424e7f6970628b2cc74a62cdf42c2121da2a
quagrainie1 <e.quagraini@alustudent.com>
2026-07-27 09:17:25 +0200

    Fix PEP8 in 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,7 +11,9 @@ def number_of_subscribers(subreddit):
     for a given subreddit. If the subreddit is invalid, returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/user)"
+    }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
